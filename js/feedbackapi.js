f284f87212728d47688317f85c03eebb92d840fd
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2023-08-02 02:52:28 +0700

    feedback on local done

--- js/feedbackapi.js
+++ js/feedbackapi.js
@@ -1,22 +1,19 @@
-document.getElementById('form').addEventListener('submit', function(event) {
+document.getElementById('feedback-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    var name = document.getElementById('name').name;
     var email = document.getElementById('email').value;
     var subject = document.getElementById('subject').value;
     var message = document.getElementById('message').value;
 
     var data = {
-        name: name,
         email: email,
         subject: subject,
         message: message
     };
 
-    axios.post('http://localhost:3000', data)
+    axios.post('http://localhost:5000/feedback', data)
         .then(function(response) {
             alert(response.data);
-            document.getElementById('name').name = '';
             document.getElementById('email').value = '';
             document.getElementById('subject').value = '';
             document.getElementById('message').value = '';
